f7001a9b6de44c69ddcfeefd248146d2b99b1ce5
tautau <tautau@one.co.il>
2014-02-09 00:47:59 +0200

    Added bootstrap

---
+++ include/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+
+	if (!defined("__WEIRDOS__")) { die(); }
+
+?>
--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
 	define('__WEIRDOS__', 'weirdos'); // Main entry point lock
+	
+	require_once 'include/bootstrap.php';
+	
+	echo "Hello world!";
 
 ?>
